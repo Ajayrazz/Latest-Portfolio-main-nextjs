fde4dfe8708df1a4d3e2e39bd980c813510f9142
Ajay Razz <ajayrazz854334@gmail.com>
2025-04-19 09:49:24 +0530

    changes made

---
+++ data/index.ts
@@ -0,0 +1,331 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Work Experience", link: "#work" },
+    { name: "Coding Profiles", link: "#code" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building  a competitive programming platform",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+       
+    {
+      id: 1,
+      title: "Hire Me",
+      des: "A full-stack job portal for job search, applications, and recruiter postings.",
+      img: "/hire.png",
+      iconLists: ["/react.jfif","/cloudName.svg","/js.png", "/tail.svg","/html.png","/vercel.svg"],
+      link: "https://hireeme.vercel.app/",
+    },
+  {
+      id: 10,
+      title: "Snappy",
+      des: "A real-time chat application with secure authentication and end-to-end encryption.",
+      img: "/snapy.png",
+      iconLists: ["/react.jfif","/js.png","/html.png", "/css.jpeg","/vercel.svg"],
+      link: "https://snapppy.vercel.app/",
+    }, 
+
+    {
+      id: 9,
+      title: "Maze Solver",
+      des: "An interactive maze generator and solver with multiple algorithms.",
+      img: "/path.png",
+      iconLists: ["/js.png","/css.jpeg","/html.png","/vercel.svg"],
+      link: "https://mazesolve.vercel.app/",
+    },
+    {
+      id: 11,
+      title: "Mini Anon AI",
+      des: "A real-time AI assistant with chat history, and API integrations.",
+      img: "/help.png",
+      iconLists: ["/react.jfif","/js.png","/html.png", "/css.jpeg","/vercel.svg"],
+      link: "https://mini-anon-ai.vercel.app/",
+    },     
+    {
+      id: 12,
+      title: "LinkHub",
+      des: "List all your links on one website.",
+      img: "/linkhub.jpg",
+      iconLists: ["/js.png","/html.png", "/css.jpeg","/vercel.svg"],
+      link: "https://link-hub-cyan.vercel.app/",
+    }, 
+
+    {
+      id: 2,
+      title: "NextGen",
+      des: "A sleek, responsive website for selling AI software seamlessly.",
+      img: "/nextgen.png",
+      iconLists: ["/next.jfif","/react.jfif","/ts.svg", "/css.jpeg","/vercel.svg"],
+      link: "https://nextgen-tau.vercel.app/",
+    },
+    {
+      id: 13,
+      title: "EazyEatz",
+      des: "An easy-to-use Online food ordering website and placing orders.",
+      img: "/food.jpg",
+      iconLists: ["/js.png","/html.png", "/css.jpeg","/vercel.svg"],
+      link: "https://eazy-eatz-pink.vercel.app/",
+    }, 
+
+    {
+      id: 7,
+      title: "ENotes",
+      des: "A secure, responsive platform for effortless academic resource sharing.",
+      img: "/enotes.png",
+      iconLists: ["/php.jfif","/mysql.jfif","/html.png", "/css.jpeg"],
+      link: "https://github.com/TuShArBhArDwA/ENotes",
+    },
+    {
+      id: 6,
+      title: "Raj Fashion",
+      des: "ECommerce platform with a responsive, user-friendly design.",
+      img: "/rajfash.jpg",
+      iconLists: ["/react.jfif","/html.png", "/css.jpeg","/vercel.svg"],
+      link: "https://raj-fashion.vercel.app/",
+    },
+    
+    {
+      id: 3,
+      title: "TeleScript Caller",
+      des: "Automate calls with this Python script using API requests and custom scripts.",
+      img: "/p2.png",
+      iconLists: ["python.jpeg"],
+      link: "https://github.com/TuShArBhArDwA/TeleScript-Caller",
+    },
+    {
+      id: 5,
+      title: "Loan Calculator",
+      des: "A web app for interest calculation with an intuitive UI and visual graphs.",
+      img: "/loan.jpg",
+      iconLists: ["/html.png", "/css.jpeg", "/ts.svg"],
+      link: "https://github.com/TuShArBhArDwA/Loan-Calculator-Web-Application",
+    },
+    {
+      id: 8,
+      title: "N-Queens Visualizer",
+      des: "Experience a stunning 3D visualization of the N-Queens problem.",
+      img: "/nqueen.jpg",
+      iconLists: ["/html.png", "/css.jpeg", "/js.png","/vercel.svg" ],
+      link: "https://n-queenvisualizer.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "eVoteX Voting System",
+      des: "A secure, user-friendly terminal system with encryption and real-time counting.",
+      img: "/onlinevote.jpg",
+      iconLists: ["/c.svg"],
+      link: "https://github.com/TuShArBhArDwA/eVoteX-Online-Voting-System",
+    },
+  ];
+  
+
+  export const articles = [
+    {
+      id: 1,
+      img: "/oneshotarticle.png",
+      title: "One-Shot Interview Prep Guide",
+      des:
+        "A quick and focused revision guide covering DSA, System Design, DBMS, OS, CN & more for last-minute interview prep.",
+      link: "https://www.linkedin.com/pulse/your-one-stop-revision-resource-cracking-software-roles-bhardwaj-zqbgc/",
+      iconLists: ["/linkedin.jpeg"],
+    },
+    {
+      id: 2,
+      img: "/microarticle.png",
+      title: "How I Cracked the Microsoft Internship",
+      des:
+        "Step-by-step breakdown of my Microsoft interview process, preparation strategy, and key takeaways.",
+      link: "https://leetcode.com/discuss/post/5857898/microsoft-swe-intern-hyderabad-bengaluru-dofg/",
+      iconLists: ["/leetcode-logo.svg"],
+    },
+    {
+      id: 3,
+      img: "/dsaarticle.png",
+      title: "Top DSA Sheets to Follow",
+      des:
+        "A curated list of the most effective DSA question sheets for cracking MAANG/FAANG and top tech interviews — handpicked with explanations.",
+      link: "https://www.linkedin.com/pulse/top-dsa-sheets-follow-tushar-bhardwaj--vigec/",
+      iconLists: ["/linkedin.jpeg","/leetcode-logo.svg"],
+    }
+    
+  ];
+
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Website Developer",
+      desc: "Working on client projects, developing websites and bots that enhanced user experience and functionality by designing intuitive, user-friendly interfaces.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+    {
+      id: 3,
+      title: "Operations Interns",
+      desc: "Managing daily operations of cohort-based programs by optimizing processes, analyzing performance data, and collaborating with leadership to enhance program efficiency and participant support.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 2,
+      title: "Content Problem Reviewer and Solution Author - Leetcode",
+      desc: "Oversaw a quality control initiative for LeetCode problems, revising 120 problem statements and examples.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    
+    {
+      id: 2,
+      title: "Technical Head - Apna College LPU",
+      desc: "Directing the technical team in developing and implementing innovative solutions for the Apna College LPU club.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/Ajayrazz",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+      link: "https://x.com/AjayRaz18514034",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/ajay-razz/",
+    },
+  ];
